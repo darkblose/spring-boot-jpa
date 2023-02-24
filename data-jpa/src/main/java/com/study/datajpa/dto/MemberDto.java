8e85faa3abba7a433cfaffdd8fd544955c769605
darkblose <darkblose@gmail.com>
2023-02-24 22:23:03 +0900

    Day16 : Spring Data JPA

--- data-jpa/src/main/java/com/study/datajpa/dto/MemberDto.java
+++ data-jpa/src/main/java/com/study/datajpa/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package com.study.datajpa.dto;
 
+import com.study.datajpa.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,4 +10,11 @@ public class MemberDto {
     private Long id;
     private String username;
     private String teamName;
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+    }
 }
+
+
