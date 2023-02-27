1390369848efc2a47503add03728dce2e5f6196e
darkblose <darkblose@gmail.com>
2023-02-27 23:57:52 +0900

    Day18 : QueryDsl

--- querydsl/src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,35 @@
 package com.study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.entity.Hello;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
+import static com.study.querydsl.entity.QHello.hello;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
 
+    @Autowired
+    EntityManager em;
+
     @Test
     void contextLoads() {
-    }
+        Hello originHello = new Hello();
+        em.persist(originHello);
 
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        Hello result = query
+                .selectFrom(hello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(originHello);
+        assertThat(result.getId()).isEqualTo(originHello.getId());
+    }
 }
